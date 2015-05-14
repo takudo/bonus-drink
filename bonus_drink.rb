8980584c0bcece7580cc5166533bb45df5b340a1
takudo <taku.doyao@gmail.com>
2015-05-14 22:17:06 +0900

    add logic and spec

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,27 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+
+    # 飲める本数
+    can_drink = amount
+
+    while amount >= 3
+
+      # 交換するので3本減る
+      amount -= 3
+
+      # 交換して１本もらえる
+      amount += 1
+
+      # 飲める本数に加算
+      can_drink += 1
+
+    end
+
+    can_drink
   end
+
+  def self.exchange(current_amount)
+
+  end
+
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,4 +2,25 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+
+  it "0本買ったら0本" do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+
+  it "1本買ったら1本" do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+
+  it "3本買ったら4本" do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+
+  it "11本買ったら16本" do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
+
+  it "100本買ったら149本" do
+    expect(BonusDrink.total_count_for(100)).to eq 149
+  end
+
 end
